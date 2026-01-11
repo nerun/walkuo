61ab02f7fc99ebba08c4d6814456f8f0dacebc80
nerun <danieldiasr@gmail.com>
2026-01-11 16:19:29 -0300

    Rebrand copyright notices across codebase

--- Server/AggressorInfo.cs
+++ Server/AggressorInfo.cs
@@ -1,23 +1,22 @@
-/***************************************************************************
- *                              AggressorInfo.cs
- *                            -------------------
- *   begin                : May 1, 2002
- *   copyright            : (C) The RunUO Software Team
- *   email                : info@runuo.com
+/* ***************************************************************************
+ * AggressorInfo.cs
  *
- *   $Id$
+ * RunUO is an open-source server emulator for Ultima Online.
+ * Copyright (C) 2002  The RunUO Software Team
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
  ***************************************************************************/
-
-/***************************************************************************
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; either version 2 of the License, or
- *   (at your option) any later version.
- *
- ***************************************************************************/
-
 using System;
 using System.IO;
 using System.Collections;
