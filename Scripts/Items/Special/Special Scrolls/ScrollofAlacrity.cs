167507cf5e93df778546294b6a08490021726984
nerun <danieldiasr@gmail.com>
2026-01-11 16:19:29 -0300

    Rebrand copyright notices across codebase

--- Scripts/Items/Special/Special Scrolls/ScrollofAlacrity.cs
+++ Scripts/Items/Special/Special Scrolls/ScrollofAlacrity.cs
@@ -1,23 +1,22 @@
-/***************************************************************************
-*							ScrollofAlacrity.cs
-*							-------------------
-*	begin				: June 1, 2009
-*	copyright			: (C) Shai'Tan Malkier aka Callandor2k
-*	email				: ShaiTanMalkier@gmail.com
-*
-*	$Id: ScrollofAlacrity.cs 1 2009-06-1 04:28:39Z Callandor2k $
-*
-***************************************************************************/
-
-/***************************************************************************
-*
-*	This Script/File is free software; you can redistribute it and/or modify
-*	it under the terms of the GNU General Public License as published by
-*	the Free Software Foundation; either version 2 of the License, or
-*	(at your option) any later version.
-*
-***************************************************************************/
-
+/* ***************************************************************************
+ * ScrollofAlacrity.cs
+ *
+ * RunUO is an open-source server emulator for Ultima Online.
+ * Copyright (C) 2009  The RunUO Software Team
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, see <https://www.gnu.org/licenses/>.
+ ***************************************************************************/
 using System;
 using Server;
 using Server.Gumps;
